565298ac24335ad2b74f626c83dfeba319493607
atyiadam <atyiadam@gmail.com>
2019-08-09 19:17:49 +0200

    Filepath change

--- algos/filepaths.jl
+++ algos/filepaths.jl
@@ -1,9 +1,9 @@
 # For Adam's windows laptop
-directed_weight_v = "personal/repos/tdk-graphalytics/example-directed/example-directed.v"
-directed_weight_e = "personal/repos/tdk-graphalytics/example-directed/example-directed.e"
+directed_weight_v = "../example-directed/example-directed.v"
+directed_weight_e = "../example-directed/example-directed.e"
 directed_unweight_v ="personal/repos/tdk-graphalytics/example-directed/example-unweighted.v"
 directed_unweight_e = "personal/repos/tdk-graphalytics/example-directed/example-unweighted.e"
-undirected_weight_v = "personal/repos/tdk-graphalytics/example-undirected/example-undirected.v"
-undirected_weight_e = "personal/repos/tdk-graphalytics/example-undirected/example-undirected.e"
+undirected_weight_v = "../example-undirected/example-undirected.v"
+undirected_weight_e = "../example-undirected/example-undirected.e"
 undirected_unweight_v = "personal/repos/tdk-graphalytics/example-undirected/example-unweighted.v"
 undirected_unweight_e = "personal/repos/tdk-graphalytics/example-undirected/example-unweighted.e"
--- algos/mylcc.jl
+++ algos/mylcc.jl
@@ -2,20 +2,25 @@ using LightGraphs
 using GraphPlot
 
 function mylcc(graph::AbstractGraph)
-
+    # Array that stores the outpot of the LCC algorithm for each vertex
     lcc = []
+    # Iterates through vertices of the graph pushing its LCC(vertex) in the lcc[] array
     for vertex in vertices(graph)
-
+        # Array of one vertex's neighbors
         neighs = unique!(vcat(inneighbors(graph,vertex),outneighbors(graph,vertex)))
+        # Degree of a vertex / if directed i->j j->i counted once
         Nv = length(neighs)
+        # Trianle count
         tcount = 0
         @show vertex neighs Nv
 
+        # If a vertex has les than 2 neighbors its pushing 0.0 as its LCC(vertex) value
         if length(neighs)<=1
             push!(lcc,0.0)
             continue
         end
 
+        # Itarets through the neigbors of neighbors
         for i in neighs
             neighs_neighs = outneighbors(graph,i)
             for j in neighs_neighs
