871bd2d9e44f323920d0d5e580cc38def794292f
Atyi Ádám <atyiadam@gmail.com>
2020-04-26 15:25:51 +0200

    Update lg bfs

--- algos/ldbc_lg_bfs.jl
+++ algos/ldbc_lg_bfs.jl
@@ -1,7 +1,7 @@
-using GraphPlot
 using LightGraphs
 
-function mybfs(graph_vertices,starting_vertex)
+# LightGraphs does not implement bfs in a way LDBC defines the returnable value
+function ldbc_lg_bfs(graph_vertices,starting_vertex)
 
     result = map(gdistances(graph_vertices[1],starting_vertex)) do x
         if x == 9223372036854775807
@@ -14,7 +14,3 @@ function mybfs(graph_vertices,starting_vertex)
     return result
 
 end
-
-
-example = createDirGraph(false,false,undirected_weight_v, undirected_weight_e)
-println(mybfs(example,1))
