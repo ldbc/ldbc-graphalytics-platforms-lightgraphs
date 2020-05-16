3ef7452b963fd28039716015f3dda2c7554d471f
Atyi Ádám <atyiadam@gmail.com>
2020-05-16 19:59:18 +0200

    Add BreadthFirstSearchJob

--- src/main/java/science/atlarge/graphalytics/julia/algorithms/bfs/BreadthFirstSearchJob.java
+++ src/main/java/science/atlarge/graphalytics/julia/algorithms/bfs/BreadthFirstSearchJob.java
@@ -1,4 +1,24 @@
 package science.atlarge.graphalytics.julia.algorithms.bfs;
 
-public class BreadthFirstSearchJob {
+import science.atlarge.graphalytics.domain.algorithms.BreadthFirstSearchParameters;
+import science.atlarge.graphalytics.domain.graph.Graph;
+import science.atlarge.graphalytics.execution.RunSpecification;
+import science.atlarge.graphalytics.julia.algorithms.JuliaConfiguration;
+import science.atlarge.graphalytics.julia.algorithms.JuliaJob;
+
+public class BreadthFirstSearchJob extends JuliaJob {
+    public BreadthFirstSearchJob(RunSpecification runSpecification, JuliaConfiguration platformConfig, String inputPath, String outputPath, Graph benchmarkGraph) {
+        super(runSpecification, platformConfig, inputPath, outputPath, benchmarkGraph);
+    }
+
+    @Override
+    protected void appendAlgorithmParameters() {
+        commandLine.addArgument("--algorithm");
+        commandLine.addArgument("bfs");
+
+        BreadthFirstSearchParameters params =
+                (BreadthFirstSearchParameters) runSpecification.getBenchmarkRun().getAlgorithmParameters();
+        commandLine.addArgument("--source-vertex");
+        commandLine.addArgument(Long.toString(params.getSourceVertex()));
+    }
 }
