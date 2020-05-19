867a4eede1ce010c746a0a47e3797e05057217a9
Atyi Ádám <atyiadam@gmail.com>
2020-05-19 14:21:01 +0200

    Edit job packages

--- src/main/java/science/atlarge/graphalytics/julia/algorithms/bfs/BreadthFirstSearchJob.java
+++ src/main/java/science/atlarge/graphalytics/julia/algorithms/bfs/BreadthFirstSearchJob.java
@@ -3,8 +3,8 @@ package science.atlarge.graphalytics.julia.algorithms.bfs;
 import science.atlarge.graphalytics.domain.algorithms.BreadthFirstSearchParameters;
 import science.atlarge.graphalytics.domain.graph.Graph;
 import science.atlarge.graphalytics.execution.RunSpecification;
-import science.atlarge.graphalytics.julia.algorithms.JuliaConfiguration;
-import science.atlarge.graphalytics.julia.algorithms.JuliaJob;
+import science.atlarge.graphalytics.julia.JuliaConfiguration;
+import science.atlarge.graphalytics.julia.JuliaJob;
 
 public class BreadthFirstSearchJob extends JuliaJob {
     public BreadthFirstSearchJob(RunSpecification runSpecification, JuliaConfiguration platformConfig, String inputPath, String outputPath, Graph benchmarkGraph) {
--- src/main/java/science/atlarge/graphalytics/julia/algorithms/lcc/LocalClusteringCoefficientJob.java
+++ src/main/java/science/atlarge/graphalytics/julia/algorithms/lcc/LocalClusteringCoefficientJob.java
@@ -2,8 +2,8 @@ package science.atlarge.graphalytics.julia.algorithms.lcc;
 
 import science.atlarge.graphalytics.domain.graph.Graph;
 import science.atlarge.graphalytics.execution.RunSpecification;
-import science.atlarge.graphalytics.julia.algorithms.JuliaConfiguration;
-import science.atlarge.graphalytics.julia.algorithms.JuliaJob;
+import science.atlarge.graphalytics.julia.JuliaConfiguration;
+import science.atlarge.graphalytics.julia.JuliaJob;
 
 public class LocalClusteringCoefficientJob extends JuliaJob {
     public LocalClusteringCoefficientJob(RunSpecification runSpecification, JuliaConfiguration platformConfig, String inputPath, String outputPath, Graph benchmarkGraph) {
