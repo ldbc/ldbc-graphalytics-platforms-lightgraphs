2bee80349401678012d5bf3a50183387a84890b3
Atyi Ádám <atyiadam@gmail.com>
2020-05-19 12:14:15 +0200

    Add JuliaCollector  / ProcTimeLog

---
+++ src/main/java/science/atlarge/graphalytics/julia/algorithms/ProcTimeLog.java
@@ -0,0 +1,15 @@
+package science.atlarge.graphalytics.julia.algorithms;
+
+import java.time.Instant;
+
+public class ProcTimeLog {
+    static final String START_PROC_TIME = "Processing starts at";
+    static final String END_PROC_TIME = "Processing ends at";
+
+    public static void start() {
+        System.out.println(START_PROC_TIME + " " + Instant.now().toEpochMilli());
+    }
+    public static void end() {
+        System.out.println(END_PROC_TIME + " " + Instant.now().toEpochMilli());
+    }
+}
