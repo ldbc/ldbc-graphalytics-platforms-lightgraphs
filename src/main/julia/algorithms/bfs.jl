97a81b3aad4d7a34b706386eb2050bf628a0a11e
Atyi Ádám <atyiadam@gmail.com>
2020-05-27 18:25:28 +0200

    add bfs dict

--- src/main/julia/algorithms/bfs.jl
+++ src/main/julia/algorithms/bfs.jl
@@ -10,11 +10,19 @@ function bfs()
     # edge_file = string(parameters["dataset"], "/example-directed.e")
     # vertex_file = string(parameters["dataset"], "/example-directed.v")
     # A = createDirGraph(parameters["directed"], false, vertex_file, edge_file)
-    A = loadgraph("/Users/adamatyi/graphs/example-directed.txt", "graph", GraphIO.EdgeList.EdgeListFormat())
-    println(A)
+    (G, forwardmapping) = loadgraph("/Users/adamatyi/graphs/example-directed.txt", "graph", GraphIO.EdgeList.EdgeListFormat())
+
+    forwardmapping = Dict(parse(Int, k) => v  for (k,v) in pairs(forwardmapping))
+    backwardmapping = Dict(forwardmapping[k] => k for k in keys(forwardmapping))
+    source_vertex_mapped = forwardmapping[parameters["source_vertex"]]
+
     println("Algorithm started...")
-    result = @time ldbc_lg_bfs(A, parameters["source_vertex"])
-    println(result)
+    result = @time ldbc_lg_bfs(G, source_vertex_mapped)
+    result_dict = Dict()
+    for i in 1 : length(result)
+        result_dict[backwardmapping[i]] = result[i]
+    end
+    println(result_dict)
 end
 
 bfs()
