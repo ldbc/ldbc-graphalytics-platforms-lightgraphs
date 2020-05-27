57bec9dd0786757a07d48ed6e6e1db0278ed7640
Atyi Ádám <atyiadam@gmail.com>
2020-05-27 18:55:11 +0200

    add prints

--- src/main/julia/algorithms/bfs.jl
+++ src/main/julia/algorithms/bfs.jl
@@ -10,10 +10,14 @@ function bfs()
     # edge_file = string(parameters["dataset"], "/example-directed.e")
     # vertex_file = string(parameters["dataset"], "/example-directed.v")
     # A = createDirGraph(parameters["directed"], false, vertex_file, edge_file)
+    println("Loading graph...")
     (G, forwardmapping) = loadgraph(parameters["dataset"], "graph", GraphIO.EdgeList.EdgeListFormat())
 
+    println("Forward mapping...")
     forwardmapping = Dict(parse(Int, k) => v  for (k,v) in pairs(forwardmapping))
+    println("Backward mapping...")
     backwardmapping = Dict(forwardmapping[k] => k for k in keys(forwardmapping))
+    
     source_vertex_mapped = forwardmapping[parameters["source_vertex"]]
 
     println("Algorithm started...")
